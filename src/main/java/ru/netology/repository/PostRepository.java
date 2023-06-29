5886f60e7d0efa5a7e23fb663b5b4b39513ed86a
Повелитель <hotabosh@mail.ru>
2023-06-29 23:31:37 +0300

    1.0

---
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -0,0 +1,38 @@
+package ru.netology.repository;
+
+import ru.netology.exception.NotFoundException;
+import ru.netology.model.Post;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+// Stub
+public class PostRepository {
+  ConcurrentHashMap<Long,Post> data = new ConcurrentHashMap<>();
+  AtomicLong atomicId = new AtomicLong();
+  public List<Post> all() {
+    return (List<Post>) data.values();
+  }
+
+  public Optional<Post> getById(long id) {
+    return Optional.ofNullable(data.get(id));
+  }
+
+  public Post save(Post post) {
+    if(post.getId()==0){
+      long id = atomicId.getAndIncrement();
+      post.setId(id);
+      data.put(id,post);
+    }else if (post.getId()!=0 && data.containsKey(post.getId())){
+      data.replace(post.getId(),post);
+    } else {
+      throw new NotFoundException("Нельзя добавлять эллемент с id < 0 и присваивать собственное значение для нового поста");
+    }
+    return post;
+  }
+
+  public void removeById(long id) {
+      data.remove(id);
+  }
+}
